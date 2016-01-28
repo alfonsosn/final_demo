a7c6b818af5ad106491a7b72bcb6c8c73aa7ea09
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2016-01-27 20:33:01 -0500

    made changes for heroku deployment

--- start.js
+++ start.js
@@ -2,9 +2,3 @@
 var server = require('./server');
 
 var PORT = 8080;
-
-mongoose.connection.once('open', function () {
-    server.listen(PORT, function () {
-	    console.log('Server started on port ' + PORT.toString());
-	});
-});
